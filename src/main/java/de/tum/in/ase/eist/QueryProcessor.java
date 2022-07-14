f5cf806103c52cbf14fd4faf827207b6f83fe6cb
Linus Weigand <linusweigand@protonmail.com>
2022-07-14 13:29:37 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -2,6 +2,8 @@ package de.tum.in.ase.eist;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class QueryProcessor {
 
@@ -26,6 +28,27 @@ public class QueryProcessor {
             }
             return "";
         } else if(query.contains("which of the following numbers is the largest:")) {
+            //Exampe string: which of the following numbers is the largest: 10, 11, 33, 45
+            String[] parts = query.split(" ");
+            for(int i = 0; i < parts.length; i++) {
+                if(parts[i].equals("largest:")) {
+                    ArrayList<Integer> numbers = new ArrayList<>();
+                    for(int j = i+1; j < parts.length; j++) {
+                        //Trim all the whitespaces and commas
+                        parts[j] = parts[j].replaceAll("\\s", "");
+                        parts[j] = parts[j].replaceAll(",", "");
+                        numbers.add(Integer.parseInt(parts[j]));
+                    }
+                    //Find the largest number
+                    int largest = numbers.get(0);
+                    for(int j = 1; j < numbers.size(); j++) {
+                        if(numbers.get(j) > largest) {
+                            largest = numbers.get(j);
+                        }
+                    }
+                    return String.valueOf(largest);
+                }
+            }
             return "";
         }else {
             return "";
